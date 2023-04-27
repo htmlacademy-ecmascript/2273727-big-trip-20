257285d793c8fb3129f5ae466fee1ad2606f06d9
EgorPestov <egorpestov2012@yandex.ru>
2023-04-27 21:52:31 +0400

    feat: added trip info section

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import FilterView from './view/filter-view.js';
+import TripInfoView from './view/trip-info-view.js';
+import TripInfoMainView from './view/trip-info-main-view.js';
+import TripInfoCostView from './view/trip-info-cost-view.js';
 import TripPlanPresenter from './presenter/trip-plan-presenter.js';
-import {render} from './render.js';
+import {RenderPosition, render} from './render.js';
 
-const filtersContainer = document.querySelector('.trip-controls__filters');
+const tripMainContainer = document.querySelector('.trip-main');
+const filtersContainer = tripMainContainer.querySelector('.trip-controls__filters');
 const tripPlanContainer = document.querySelector('.trip-events');
 const tripPlanPresenter = new TripPlanPresenter({tripPlanContainer: tripPlanContainer});
 
+render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
+const tripInfoContainer = document.querySelector('.trip-main__trip-info');
+render(new TripInfoMainView, tripInfoContainer);
+render(new TripInfoCostView, tripInfoContainer);
 render(new FilterView(), filtersContainer);
 
 tripPlanPresenter.init();
