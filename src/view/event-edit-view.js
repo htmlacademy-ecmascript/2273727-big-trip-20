eec1227287336bf6d23f02db4ad75c8ab59364d4
EgorPestov <egorpestov2012@yandex.ru>
2023-06-01 13:33:32 +0400

    fix: changed type of input for price for number

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -116,7 +116,7 @@ function createEventEditTemplate(state, destinations, offers) {
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${basePrice}">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit" ${isSubmitDisabled ? 'disabled' : ''}>Save</button>
