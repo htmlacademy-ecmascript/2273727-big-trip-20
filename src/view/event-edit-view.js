bbf04719daacc706c6672918af7ccc4af98fcf88
EgorPestov <egorpestov2012@yandex.ru>
2023-06-10 09:43:23 +0400

    fix: destination now is required field

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -95,7 +95,7 @@ function createEventEditTemplate(state, destinations, offers) {
           <label class="event__label  event__type-output" for="event-destination-1">
             ${type}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${isEventJustOpen ? '' : destination.name}" list="destination-list-1" ${isDisabled ? 'disabled' : ''}>
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${isEventJustOpen ? '' : destination.name}" list="destination-list-1" ${isDisabled ? 'disabled' : ''} required>
           <datalist id="destination-list-1">
             ${createDestinationsTemplate()}
           </datalist>
